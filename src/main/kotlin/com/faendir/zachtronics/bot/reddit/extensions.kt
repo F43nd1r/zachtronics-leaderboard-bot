ae20d36b5e944cce22ee9b8c0b0d6c6334b4915c
f43nd1r <lukas.morawietz@gmail.com>
2020-09-19 01:45:16 +0200

    deal with 429 responses from reddit

--- src/main/kotlin/com/faendir/zachtronics/bot/reddit/extensions.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/reddit/extensions.kt
@@ -5,10 +5,22 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import net.dean.jraw.references.SubredditReference
+import java.io.IOException
 import java.net.URL
 
 fun SubredditReference.moderators(): List<String> {
-    val response: Response = Json { ignoreUnknownKeys = true }.decodeFromString(URL("https://www.reddit.com/r/$subreddit/about/moderators.json").readText())
+    var text: String? = null
+    while (text == null) {
+        try {
+            text = URL("https://www.reddit.com/r/$subreddit/about/moderators.json").readText()
+        } catch (e: IOException) {
+            if (e.message?.contains("429") == true) {
+                //API overload, wait a bit
+                Thread.sleep(1000 * 60)
+            } else throw e
+        }
+    }
+    val response: Response = Json { ignoreUnknownKeys = true }.decodeFromString(text)
     check(response.kind == "UserList")
     return response.data.children.map { it.name }
 }
