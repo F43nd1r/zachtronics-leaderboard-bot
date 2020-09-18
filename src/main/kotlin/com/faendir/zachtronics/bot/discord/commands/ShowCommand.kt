5f7742d7450bb03baf009148c6d948a2dcb6ef29
f43nd1r <lukas.morawietz@gmail.com>
2020-09-18 21:32:38 +0200

    fix category selection in show command

--- src/main/kotlin/com/faendir/zachtronics/bot/discord/commands/ShowCommand.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/discord/commands/ShowCommand.kt
@@ -19,26 +19,25 @@ class ShowCommand : Command {
     override fun <S : Score<S, *>, P : Puzzle> handleMessage(game: Game<S, P>, leaderboards: List<Leaderboard<*, S, P>>, author: User, channel: TextChannel, message: Message,
                                                              command: MatchResult): String {
         val categoryString = command.groups["category"]!!.value
-        val leaderBoardCategoryPair = leaderboards.flatMap { it.pairWithEachCategory() }.find { pair -> pair.category.displayName.equals(categoryString, ignoreCase = true) }
-            ?: return "sorry, I could not find the category \"$categoryString\""
-        return handleMessageImpl(game, command, leaderBoardCategoryPair)
+        return findPuzzle(game, command.groups["puzzle"]!!.value) { puzzle ->
+            val leaderBoardCategoryPair = leaderboards.flatMap { it.pairWithEachCategory() }
+                .filter { pair -> pair.category.displayName.equals(categoryString, ignoreCase = true) }
+                .ifEmpty { return@findPuzzle "sorry, I could not find the category \"$categoryString\"" }
+                .filter { it.category.supportedTypes.contains(puzzle.type) }
+                .ifEmpty { return@findPuzzle "sorry, the category \"${categoryString.toLowerCase()}\" does not support puzzles of type ${puzzle.type.displayName}." }
+                .find { it.category.supportedGroups.contains(puzzle.group) }
+                ?: return@findPuzzle "sorry, the category \"${categoryString.toLowerCase()}\" does not support puzzles in ${puzzle.group.displayName}."
+            return@findPuzzle handleMessageImpl(puzzle, leaderBoardCategoryPair)
+        }
     }
 
-    private fun <C : Category<C, *, *>, P : Puzzle> handleMessageImpl(game: Game<*, P>, command: MatchResult, leaderBoardCategoryPair: LeaderBoardCategoryPair<C, P>): String {
+    private fun <C : Category<C, *, *>, P : Puzzle> handleMessageImpl(puzzle: P, leaderBoardCategoryPair: LeaderBoardCategoryPair<C, P>): String {
         val (leaderboard, category) = leaderBoardCategoryPair
-        return findPuzzle(game, command.groups["puzzle"]!!.value) { puzzle ->
-            if (!category.supportedTypes.contains(puzzle.type)) {
-                "sorry, the category ${category.displayName} does not support puzzles of type ${puzzle.type.displayName}."
-            } else if (!category.supportedGroups.contains(puzzle.group)) {
-                "sorry, the category ${category.displayName} does not support puzzles in ${puzzle.group.displayName}."
-            } else {
-                leaderboard.get(puzzle, category)?.let {
-                    "here you go: ${puzzle.displayName} ${category.displayName} ${
-                        it.score.reorderToStandard().toString("/")
-                    } ${it.link}"
-                } ?: "sorry, there is no score for ${puzzle.displayName} ${category.displayName}."
-            }
-        }
+        return leaderboard.get(puzzle, category)?.let {
+            "here you go: ${puzzle.displayName} ${category.displayName} ${
+                it.score.reorderToStandard().toString("/")
+            } ${it.link}"
+        } ?: "sorry, there is no score for ${puzzle.displayName} ${category.displayName}."
     }
 }
 
