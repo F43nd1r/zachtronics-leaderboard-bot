8b36c395cd178f5f5e576fceae066b3670b27db8
f43nd1r <lukas.morawietz@gmail.com>
2020-09-21 14:46:48 +0200

    be more precise. Closes #4

--- src/main/kotlin/com/faendir/zachtronics/bot/discord/commands/utils.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/discord/commands/utils.kt
@@ -8,7 +8,7 @@ fun <P : Puzzle> findPuzzle(game: Game<*, *, P>, puzzleName: String, processPuzz
     return when (val size = puzzles.size) {
         0 -> "sorry, I did not recognize the puzzle \"$puzzleName\"."
         1 -> processPuzzle(puzzles.first())
-        in 2..5 -> "sorry, your request for \"$puzzleName\" was not accurate enough. Use one of:\n${puzzles.joinToString("\n") { it.displayName }}"
-        else -> "sorry, your request for \"$puzzleName\" was not accurate enough. $size matches."
+        in 2..5 -> "sorry, your request for \"$puzzleName\" was not precise enough. Use one of:\n${puzzles.joinToString("\n") { it.displayName }}"
+        else -> "sorry, your request for \"$puzzleName\" was not precise enough. $size matches."
     }
 }
