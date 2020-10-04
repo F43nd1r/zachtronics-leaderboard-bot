97ce00e958a4711ad4f9faab86f2aea7920f6d6d
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2020-10-04 04:48:09 +0200

    Fill out SpaceChem's help

--- src/main/java/com/faendir/zachtronics/bot/model/sc/SpaceChem.java
+++ src/main/java/com/faendir/zachtronics/bot/model/sc/SpaceChem.java
@@ -1,52 +1,70 @@
 package com.faendir.zachtronics.bot.model.sc;
 
 import com.faendir.zachtronics.bot.leaderboards.Leaderboard;
-import com.faendir.zachtronics.bot.leaderboards.sc.ScRedditLeaderboard;
 import com.faendir.zachtronics.bot.model.Game;
 import com.faendir.zachtronics.bot.utils.Result;
 import com.faendir.zachtronics.bot.utils.UtilsKt;
 import kotlin.Pair;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class SpaceChem implements Game<ScCategory, ScScore, ScPuzzle, ScRecord> {
     @Getter
     private final String discordChannel = "spacechem";
     @Getter
-    private final String displayName = "Spacechem";
+    private final String displayName = "SpaceChem";
     @Getter
     private final List<Leaderboard<ScCategory, ScScore, ScPuzzle, ScRecord>> leaderboards;
     @Getter
-    private final String submissionSyntax =
-            "<puzzle> (<cycles/reactors/symbols[/BP]>) by <author> <youtube link>";
+    private final String submissionSyntax = "<puzzle> (<cycles/reactors/symbols[/BP]>) by <author> <youtube link>";
     @Getter
-    private final String categoryHelp = "TODO";
+    private final String categoryHelp = "The supported categories for researches are Cycles and Symbols, " +
+                                        "ties broken by the other metric.\n" +
+                                        "For productions we have in addition Cycles and Symbols at minimum reactors.\n" +
+                                        "Boss levels are currently not supported.\n" +
+                                        "\n" +
+                                        "Each category also comes in a \"No bugs allowed\" and a " +
+                                        "\"No precognition allowed\" version (for random puzzles), " +
+                                        "whose record is very often equal to the unrestricted record.\n" +
+                                        "\n" +
+                                        "For further information see: " +
+                                        "<https://www.reddit.com/r/spacechem/wiki/index#wiki_explanations>\n" +
+                                        "\n" +
+                                        "All categories are:\n" +
+                                        Arrays.stream(ScCategory.values())
+                                              .map(c -> c.getDisplayName() + " (" + c.getContentDescription() + ")")
+                                              .collect(Collectors.joining("\n", "```", "```"));
     @Getter
-    private final String scoreHelp = "TODO";
+    private final String scoreHelp = "Scores are in the format cycles/reactors/symbols.\n" +
+                                     "To mark a score as bugged in a submission append a `/B`, (`c/r/s/B`), " +
+                                     "`/P` for precognition, `/BP` for both.";
 
-    public SpaceChem(ScRedditLeaderboard scLeaderboard) {
-        leaderboards = Collections.singletonList(scLeaderboard);
-    }
-
-    private static final Pattern submissionRegex = Pattern.compile(
-            "!submit\\s+(?<puzzle>.+)\\s+\\((?<score>" + ScScore.REGEX_BP_SCORE +
-            ")\\)\\s+(?:by\\s+)?(?<author>.+?)\\s+(?<link>\\S+)\\s*",
+    private static final Pattern SUBMISSION_REGEX = Pattern.compile(
+            "!submit\\s+" +
+            "(?<puzzle>.+)\\s+" +
+            "\\((?<score>" + ScScore.REGEX_BP_SCORE + ")\\)\\s+" +
+            "(?:by\\s+)?(?<author>.+?)\\s+" +
+            "(?<link>\\S+)\\s*",
             Pattern.CASE_INSENSITIVE);
 
     @NotNull
     @Override
     public Result<Pair<ScPuzzle, ScRecord>> parseSubmission(@NotNull Message message) {
-        Matcher m = submissionRegex.matcher(message.getContentRaw());
+        Matcher m = SUBMISSION_REGEX.matcher(message.getContentRaw());
         if (!m.matches())
             return Result.failure("Couldn't parse request");
 
