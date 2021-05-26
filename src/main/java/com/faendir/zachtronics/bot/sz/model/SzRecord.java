5b48d68fbf297c48eb7f96bd581b4f688af65c64
f43nd1r <account+github@faendir.com>
2021-05-27 00:32:41 +0200

    sz attachments

--- src/main/java/com/faendir/zachtronics/bot/sz/model/SzRecord.java
+++ src/main/java/com/faendir/zachtronics/bot/sz/model/SzRecord.java
@@ -2,8 +2,16 @@ package com.faendir.zachtronics.bot.sz.model;
 
 import com.faendir.zachtronics.bot.model.Record;
 import kotlinx.serialization.Serializable;
+import lombok.SneakyThrows;
 import lombok.Value;
 import org.jetbrains.annotations.NotNull;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Collections;
+import java.util.List;
 
 @Serializable
 @Value
@@ -11,4 +19,17 @@ public class SzRecord implements Record {
     SzScore score;
     String author;
     String link;
+
+    @NotNull
+    @Override
+    public String toDisplayString() {
+        return score.toDisplayString() + (author != null ? " by " + author : "");
+    }
+
+    @SneakyThrows
+    @NotNull
+    @Override
+    public List<Tuple2<String, InputStream>> attachments() {
+        return Collections.singletonList(Tuples.of(link.substring(link.lastIndexOf('/')), new URL(link).openStream()));
+    }
 }
