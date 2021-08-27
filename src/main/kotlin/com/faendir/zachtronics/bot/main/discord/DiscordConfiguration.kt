45453f00dd659f4d34be32d0bff6ab4990023c16
f43nd1r <account+github@faendir.com>
2021-08-27 12:41:56 +0200

    log git commit id

--- src/main/kotlin/com/faendir/zachtronics/bot/main/discord/DiscordConfiguration.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/main/discord/DiscordConfiguration.kt
@@ -7,18 +7,25 @@ import discord4j.core.`object`.presence.ClientActivity
 import discord4j.core.`object`.presence.ClientPresence
 import discord4j.gateway.intent.Intent
 import discord4j.gateway.intent.IntentSet
+import org.slf4j.LoggerFactory
 import org.springframework.boot.info.GitProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class DiscordConfiguration {
+    companion object {
+        private val logger = LoggerFactory.getLogger(DiscordService::class.java)
+    }
 
     @Bean
     fun discordClient(discordProperties: DiscordProperties, gitProperties: GitProperties): GatewayDiscordClient = DiscordClientBuilder.create(discordProperties.token)
         .build()
         .gateway()
-        .setInitialPresence { ClientPresence.online(ClientActivity.playing(gitProperties.shortCommitId)) }
+        .setInitialPresence {
+            logger.info("Connecting to discord with version ${gitProperties.shortCommitId}")
+            ClientPresence.online(ClientActivity.playing(gitProperties.shortCommitId))
+        }
         .setEnabledIntents(IntentSet.of(Intent.DIRECT_MESSAGES, Intent.GUILD_MESSAGES))
         .login()
         .block()!!
