c551f87aaaac31057eb156e265be2000b76e8b47
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2021-08-29 02:19:47 +0200

    fix solution tests

--- src/test/java/com/faendir/zachtronics/bot/sc/model/ScSolutionTest.java
+++ src/test/java/com/faendir/zachtronics/bot/sc/model/ScSolutionTest.java
@@ -14,12 +14,15 @@ class ScSolutionTest {
                                        "SOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
         ScSolution s2 = new ScSolution(new ScScore(50, 50, 50, false, false),
                                        "SOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
+        ScSolution s3 = new ScSolution(null,
+                                       "SOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
         assertNotEquals(s1, s2);
+        assertEquals(s1, s3);
     }
 
     @Test
-    public void testScoreFromContent() {
-        ScSolution s1 = new ScSolution(new ScScore(50, 50, 50, true, false),
+    public void testIncoherentContentScore() {
+        ScSolution s1 = new ScSolution(new ScScore(1, 1, 1, false, false),
                                        "SOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
         ScSolution s2 = new ScSolution(null,
                                        "SOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
@@ -27,9 +30,9 @@ class ScSolutionTest {
     }
 
     @Test
-    public void testIncoherentContentScore() {
+    public void testInvalidContent() {
         ScScore score = new ScScore(50, 50, 50, false, false);
-        assertBreaks(score, "SOLUTION:Of Pancakes and Spaceships,12345ieee,10-10-10\nstuff...");
+        assertBreaks(score, "SOLUTION:No puzzle that exists,12345ieee,50-50-50\nstuff...");
         assertBreaks(score, "stuff...\nSOLUTION:Of Pancakes and Spaceships,12345ieee,50-50-50\nstuff...");
     }
 
