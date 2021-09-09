f61f1072658bd1769880b2364fd4308785422b8b
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2021-09-09 03:51:26 +0200

    Make ArchiveResult Kotlin code because apparently the kotlin compiler isn't as smart as it thinks it is

---
+++ src/main/kotlin/com/faendir/zachtronics/bot/archive/ArchiveResult.kt
@@ -0,0 +1,11 @@
+package com.faendir.zachtronics.bot.archive
+
+/**
+ * Used to store archive result in the title string for games that don't use the title<br></br>
+ * TODO: this is horrendous
+ */
+enum class ArchiveResult(val titleString: String) {
+    FAILURE(""),
+    ALREADY_ARCHIVED("ALREADY_ARCHIVED"),
+    SUCCESS(".+");
+}
