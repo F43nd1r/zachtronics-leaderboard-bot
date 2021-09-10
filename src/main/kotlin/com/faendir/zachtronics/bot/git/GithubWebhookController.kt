6adef426052961785da3642732e2fe64729a80b1
f43nd1r <account+github@faendir.com>
2021-09-10 02:12:13 +0200

    hopefully fix sha256 validation

--- src/main/kotlin/com/faendir/zachtronics/bot/git/GithubWebhookController.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/git/GithubWebhookController.kt
@@ -1,29 +1,42 @@
 package com.faendir.zachtronics.bot.git
 
 import com.faendir.zachtronics.bot.config.GitProperties
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.util.Base64Utils
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
 
 @RestController
-class GithubWebhookController(private val repositories: List<GitRepository>, private val gitProperties: GitProperties) {
+class GithubWebhookController(private val repositories: List<GitRepository>, private val gitProperties: GitProperties, private val objectMapper: ObjectMapper) {
     companion object {
         private val logger = LoggerFactory.getLogger(GithubWebhookController::class.java)
     }
 
     @PostMapping(path = ["/push"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun reportPush(@RequestBody payload: Payload, @RequestHeader(name = "X-Hub-Signature-256") secret: String) {
-        if (secret != gitProperties.webhookSecret) {
-            logger.warn("Received webhook call with invalid secret")
+    fun reportPush(@RequestBody payloadString: String, @RequestHeader(name = "X-Hub-Signature-256") signature: String) {
+        if (signature != hash(payloadString)) {
+            logger.warn("Received webhook call with invalid signature")
             return
         }
+        val payload = objectMapper.readValue<Payload>(payloadString)
         if (payload.ref == "refs/heads/master" && payload.pusher.email != "zachtronics-leaderboard-bot@faendir.com") {
             val repository = repositories.find { it.url.equals(payload.repository.git_url, ignoreCase = true) }
             repository?.invalidate()
         }
+    }
+
+    private fun hash(payload: String): String {
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(SecretKeySpec(gitProperties.webhookSecret.toByteArray(Charsets.UTF_8), "HmacSHA256"))
+        return "sha256=" + Base64Utils.encode(mac.doFinal(payload.toByteArray(Charsets.UTF_8)))
     }
 }
 
