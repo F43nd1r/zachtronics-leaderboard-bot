d2340c96384f2315e724475bb84cf90b504a7737
f43nd1r <account+github@faendir.com>
2021-09-10 21:45:37 +0200

    handle pings

--- src/main/kotlin/com/faendir/zachtronics/bot/git/GithubWebhookController.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/git/GithubWebhookController.kt
@@ -6,11 +6,13 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.google.common.hash.Hashing
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 
 @RestController
@@ -20,20 +22,31 @@ class GithubWebhookController(private val repositories: List<GitRepository>, pri
     }
 
     @PostMapping(path = ["/push"], consumes = [MediaType.APPLICATION_JSON_VALUE])
-    fun reportPush(@RequestBody payloadString: String, @RequestHeader(name = "X-Hub-Signature-256") signature: String) {
+    fun reportPush(
+        @RequestBody payloadString: String,
+        @RequestHeader(name = "X-Hub-Signature-256") signature: String,
+        @RequestHeader(name = "X-GitHub-Event") event: String
+    ) {
         if (signature.removePrefix("sha256=") != hash(payloadString)) {
             logger.warn("Received webhook call with invalid signature")
-            return
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED)
         }
-        val payload = objectMapper.readValue<Payload>(payloadString)
-        if (payload.ref == "refs/heads/master") {
-            val repository = repositories.find { it.url.equals(payload.repository.clone_url, ignoreCase = true) }
-            if (repository != null) {
-                logger.info("invalidating ${repository.name}")
-                repository.updateRemoteHash(payload.headCommit.id)
-            } else {
-                logger.warn("received webhook for unknown repository ${payload.repository.clone_url}")
+        if (event == "push") {
+            val payload = objectMapper.readValue<Payload>(payloadString)
+            if (payload.ref == "refs/heads/master") {
+                val repository = repositories.find { it.url.equals(payload.repository.clone_url, ignoreCase = true) }
+                if (repository != null) {
+                    logger.info("invalidating ${repository.name}")
+                    repository.updateRemoteHash(payload.headCommit.id)
+                } else {
+                    logger.warn("received webhook for unknown repository ${payload.repository.clone_url}")
+                }
             }
+        } else if (event == "ping") {
+            logger.info("Received ping")
+        } else {
+            logger.warn("received unknown webhook event $event")
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
         }
     }
 
