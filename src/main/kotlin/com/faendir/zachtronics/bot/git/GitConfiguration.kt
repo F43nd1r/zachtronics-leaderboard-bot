100ea293dc4131722230af94a09e6c9216d655c2
f43nd1r <account+github@faendir.com>
2021-11-15 01:31:39 +0100

    use reddit pre- and suffix from repo again

--- src/main/kotlin/com/faendir/zachtronics/bot/git/GitConfiguration.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/git/GitConfiguration.kt
@@ -26,9 +26,6 @@ class GitConfiguration(private val gitProperties: GitProperties) {
     @Bean("omGithubPagesLeaderboardRepository")
     fun omGithubPagesLeaderboardRepository() = GitRepository(gitProperties, "om-leaderboard", "https://github.com/F43nd1r/om-leaderboard.git")
 
-    @Bean("omRedditLeaderboardRepository")
-    fun omRedditLeaderboardRepository() = GitRepository(gitProperties, "om-wiki", "https://github.com/F43nd1r/OM-wiki.git")
-
     @Bean("omArchiveRepository")
     fun omArchiveRepository() = GitRepository(gitProperties, "om-archive", "https://github.com/F43nd1r/om-archive.git")
 
@@ -37,7 +34,4 @@ class GitConfiguration(private val gitProperties: GitProperties) {
 
     @Bean("szRepository")
     fun szRepository() = GitRepository(gitProperties, "shenzhenIO-leaderboard", "https://github.com/12345ieee/shenzhenIO-leaderboard.git")
-
-    @Bean("configRepository")
-    fun configRepository() = GitRepository(gitProperties, "config", "https://github.com/F43nd1r/zachtronics-leaderboard-bot-config.git")
 }
