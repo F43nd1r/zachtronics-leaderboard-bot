6a184ed1485d0e15872c6e4316ee9d8d4fc73006
Andrea Stacchiotti <crudepatate@gmail.com>
2022-02-01 02:24:16 +0100

    sudo pls die but not die too much

--- src/main/java/com/faendir/zachtronics/bot/discord/command/RestartCommand.java
+++ src/main/java/com/faendir/zachtronics/bot/discord/command/RestartCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021
+ * Copyright (c) 2022
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,9 +17,11 @@
 package com.faendir.zachtronics.bot.discord.command;
 
 import com.faendir.discord4j.command.annotation.ApplicationCommand;
+import com.faendir.discord4j.command.annotation.Description;
 import com.faendir.zachtronics.bot.discord.command.security.DiscordUser;
 import com.faendir.zachtronics.bot.discord.command.security.DiscordUserSecured;
 import com.faendir.zachtronics.bot.discord.command.security.Secured;
+import com.faendir.zachtronics.bot.git.GitRepository;
 import discord4j.core.event.domain.interaction.DeferrableInteractionEvent;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -32,6 +34,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -42,13 +46,19 @@ public class RestartCommand implements TopLevelCommand<RestartCommand.RestartDat
     private final Secured secured = new DiscordUserSecured(DiscordUser.BOT_OWNERS);
     @Getter
     private final String commandName = "restart";
-    @Getter
+
     private final ApplicationContext applicationContext;
+    private final List<GitRepository> repositories;
 
     @SneakyThrows
     @NotNull
     @Override
-    public Mono<Void> handle(@NotNull DeferrableInteractionEvent event, RestartData parameters) {
+    public Mono<Void> handle(@NotNull DeferrableInteractionEvent event, @NotNull RestartData parameters) {
+        if (!parameters.sudo) {
+            // acquire and hold all the repo write locks, which ensures no write operations are concurrently running
+            repositories.parallelStream().forEach(GitRepository::acquireWriteAccess);
+        }
+
         return event.editReply("shutting down, see you soon!").then(Mono.fromCallable(() -> {
             log.error("Requested shut down, see you soon");
             SpringApplication.exit(applicationContext);
@@ -59,5 +69,11 @@ public class RestartCommand implements TopLevelCommand<RestartCommand.RestartDat
     }
 
     @ApplicationCommand(name = "restart", description = "Stops the bot, which will restart with the latest image")
-    public static class RestartData {}
+    public static class RestartData {
+        Boolean sudo;
+
+        public RestartData(@Description("Restarts immediately without waiting process termination") Boolean sudo) {
+            this.sudo = sudo;
+        }
+    }
 }
