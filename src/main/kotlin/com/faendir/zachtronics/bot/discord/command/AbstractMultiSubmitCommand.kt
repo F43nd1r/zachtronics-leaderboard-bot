da7493f401dae1ac3b9f6210cb77099eeaff6448
Andrea Stacchiotti <crudepatate@gmail.com>
2022-02-06 16:22:46 +0100

    Add verb to archive successes

--- src/main/kotlin/com/faendir/zachtronics/bot/discord/command/AbstractMultiSubmitCommand.kt
+++ src/main/kotlin/com/faendir/zachtronics/bot/discord/command/AbstractMultiSubmitCommand.kt
@@ -69,7 +69,7 @@ abstract class AbstractMultiSubmitCommand<T, C : Category, P : Puzzle<C>, S : Su
                 is ValidationResult.Valid<S>, is ValidationResult.Invalid<S> -> {
                     val score = validationResult.submission.score.toDisplayString(DisplayContext.discord())
                     when (submitResult) {
-                        is SubmitResult.Success -> "`$score`${validationResult.submission.author.orEmpty(prefix = " by ")}.\n${submitResult.message}"
+                        is SubmitResult.Success -> "`$score`${validationResult.submission.author.orEmpty(prefix = " by ")} was added.\n${submitResult.message}"
                         is SubmitResult.AlreadyPresent -> "`$score` was already present."
                         is SubmitResult.NothingBeaten -> "`$score` did not beat anything."
                         is SubmitResult.Failure -> "`$score` failed.\n${submitResult.message}"
