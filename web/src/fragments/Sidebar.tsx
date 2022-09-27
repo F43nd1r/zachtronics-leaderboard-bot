b79427b75550f79ebe1e769b3e24b2441e8a6070
Syx Pek <20557013+SyxP@users.noreply.github.com>
2022-09-27 17:57:51 +0800

    Update Regex to prevent expanding of subcategories.

--- web/src/fragments/Sidebar.tsx
+++ web/src/fragments/Sidebar.tsx
@@ -28,7 +28,7 @@ import { Link, useMatch } from "react-router-dom"
 import ApiResource from "../utils/ApiResource"
 
 function Campaign() {
-    const match = useMatch("/puzzles/*")
+    const match = useMatch("/puzzles/[P007|P010|P009|P011|P013|P008|P012|P014|P015|P016|P019|P018|P017|P020|P021|P022|P024|P025|P026|P027|P028|P030b|P029|P031b|P034|P033|P032|P036|P035|P037|P038|P042|P039|P040|P041|P043|P076|P080|P075|P074|P083|P078|P079|P081|P082|P077|P084]/*")
     const CampaignChapters = ["CHAPTER_1", "CHAPTER_2", "CHAPTER_3", "CHAPTER_4",
                               "CHAPTER_5", "CHAPTER_PRODUCTION"]
     return (
@@ -62,7 +62,7 @@ function Campaign() {
 }
 
 function Journal() {
-    const match = useMatch("/puzzles/*")
+    const match = useMatch("/puzzles/[P054|P055|P056|P057|P058|P059|P060|P061|P062|P063|P064|P065|P066|P067|P068|P069|P070|P071|P072|P086|P088|P085|P087|P089|P091b|P090|P092|P093|P094|P096|P095|P097|P098|P099|P100|P101|P102|P104|P103|P106|P105|P109|P108|P107]/*")
     const JournalChapters = ["JOURNAL_I", "JOURNAL_II", "JOURNAL_III", "JOURNAL_IV",
                              "JOURNAL_V", "JOURNAL_VI", "JOURNAL_VII", "JOURNAL_VIII",
                              "JOURNAL_IX"]
@@ -97,7 +97,7 @@ function Journal() {
 }
 
 function Community() {
-    const match = useMatch("/puzzles/*")
+    const match = useMatch("/puzzles/w*")
     const CampaignChapters = ["CHAPTER_1", "CHAPTER_2", "CHAPTER_3", "CHAPTER_4",
                               "CHAPTER_5", "CHAPTER_PRODUCTION"]
     const JournalChapters = ["JOURNAL_I", "JOURNAL_II", "JOURNAL_III", "JOURNAL_IV",
