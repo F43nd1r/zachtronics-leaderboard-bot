b42a3733c5dc95e431547366985359c31630c620
Andrea Stacchiotti <crudepatate@gmail.com>
2024-05-16 23:55:34 +0200

    Repository locking command, take 2

--- src/main/java/com/faendir/zachtronics/bot/discord/command/RepoCommand.java
+++ src/main/java/com/faendir/zachtronics/bot/discord/command/RepoCommand.java
@@ -19,6 +19,7 @@ package com.faendir.zachtronics.bot.discord.command;
 import com.faendir.zachtronics.bot.discord.Colors;
 import com.faendir.zachtronics.bot.discord.command.option.CommandOption;
 import com.faendir.zachtronics.bot.discord.command.option.CommandOptionBuilder;
+import com.faendir.zachtronics.bot.discord.command.option.OptionHelpersKt;
 import com.faendir.zachtronics.bot.discord.command.security.DiscordUser;
 import com.faendir.zachtronics.bot.discord.command.security.DiscordUserSecured;
 import com.faendir.zachtronics.bot.discord.command.security.NotSecured;
@@ -26,34 +27,68 @@ import com.faendir.zachtronics.bot.discord.command.security.Secured;
 import com.faendir.zachtronics.bot.discord.embed.MultiMessageSafeEmbedMessageBuilder;
 import com.faendir.zachtronics.bot.discord.embed.SafeMessageBuilder;
 import com.faendir.zachtronics.bot.git.GitRepository;
+import com.faendir.zachtronics.bot.utils.Markdown;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 
+import java.io.Closeable;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
 
 @Component
 public class RepoCommand extends Command.Group {
+    @RequiredArgsConstructor
+    private enum LockType {
+        NONE("None", r -> null),
+        WRITE("Write", GitRepository::acquireReadAccess),
+        READWRITE("ReadWrite", GitRepository::acquireWriteAccess);
+
+        @Getter
+        private final String displayName;
+        private final Function<GitRepository, GitRepository.ReadAccess> lockAction;
+
+        private @NotNull Lock makeLock(GitRepository repo) {
+            return new Lock(this, lockAction.apply(repo));
+        }
+    }
+    
+    @Value
+    static class Lock implements Closeable {
+        @NotNull LockType lockType;
+        @Nullable GitRepository.ReadAccess lock;
+
+        @Override
+        public void close() {
+            if (lock != null) {
+                lock.close();
+            }
+        }
+    }
+
     @Getter
     private final String name = "repo";
     @Getter
     private final String description = "Leaderboard repository management";
 
-    private final Map<String, GitRepository> repositories;
-    private final Map<String, GitRepository.ReadAccess> locks = new HashMap<>();
+    /** spring id -> repo */
+    private final Map<String, @NotNull GitRepository> repositories;
+    /** repo name -> lock */
+    private final Map<String, @NotNull Lock> locks;
 
     private final CommandOption<String, GitRepository> repoOption;
-    private final CommandOption<String, Class<? extends GitRepository.ReadAccess>> lockLevelOption =
-        CommandOptionBuilder.string("lockLevel")
-                            .description("Select lock type (defaults to write)")
-                            .choices(Map.of("read", "r", "readWrite", "w"))
-                            .convert((event, l) -> l.equals("r") ? GitRepository.ReadAccess.class : GitRepository.ReadWriteAccess.class)
-                            .build();
+    private final CommandOption<String, LockType> lockTypeOption =
+        OptionHelpersKt.enumOptionBuilder("locktype", LockType.class, LockType::getDisplayName)
+                       .description("Select lock type (defaults to write)")
+                       .build();
 
     @Getter
     private final List<Command.Leaf> commands;
@@ -62,120 +97,65 @@ public class RepoCommand extends Command.Group {
         // repositories come from spring, we need to init what depends on them in the constructor
         // as field initializers would run before the constructor and find a `null` repositories map
         this.repositories = repositories;
+        locks = repositories.values().stream().collect(toMap(GitRepository::getName, LockType.NONE::makeLock));
         repoOption = CommandOptionBuilder.string("repo")
                                          .description("Select repository to lock")
                                          .choices(repositories.entrySet()
                                                               .stream()
-                                                              .collect(Collectors.toMap(e -> e.getValue().getName(), Map.Entry::getKey)))
+                                                              .collect(toMap(e -> e.getValue().getName(), Map.Entry::getKey)))
                                          .convert((event, k) -> this.repositories.get(k))
                                          .required()
                                          .build();
-        commands = List.of(new InfoCommand(), new StateCommand(), new LockCommand(), new UnlockCommand());
+        commands = List.of(new StatusCommand(), new LockCommand());
     }
 
-    class InfoCommand extends Command.BasicLeaf {
-        @Getter
+    @Getter
+    class StatusCommand extends Command.BasicLeaf {
         private final Secured secured = NotSecured.INSTANCE;
-        @Getter
-        private final String name = "info";
-        @Getter
-        private final String description = "Information on game repo";
-        @Getter
-        private final List<CommandOption<?,?>> options = List.of(repoOption);
-
-        @NotNull
-        @Override
-        public SafeMessageBuilder handleEvent(@NotNull ChatInputInteractionEvent event) {
-            GitRepository repo = repoOption.get(event);
-            return new MultiMessageSafeEmbedMessageBuilder()
-                .title("Repository locks")
-                .color(Colors.READ)
-                .description(repo.getName() + "\n" + repo.getUrl());
-        }
-    }
-
-    class StateCommand extends Command.BasicLeaf {
-        @Getter
-        private final Secured secured = NotSecured.INSTANCE;
-        @Getter
-        private final String name = "state";
-        @Getter
-        private final String description = "Query state of repo locks";
-        @Getter
+        private final String name = "status";
+        private final String description = "Query status of repos and locks";
         private final List<CommandOption<?,?>> options = Collections.emptyList();
 
         @NotNull
         @Override
         public SafeMessageBuilder handleEvent(@NotNull ChatInputInteractionEvent event) {
             MultiMessageSafeEmbedMessageBuilder embed = new MultiMessageSafeEmbedMessageBuilder()
-                .title("Repository locks")
+                .title("Repositories")
                 .color(Colors.READ);
-            for (Map.Entry<String, GitRepository.ReadAccess> entry: locks.entrySet()) {
-                embed.addField(entry.getKey(), entry.getValue().getClass().getName(), false);
+            for (Map.Entry<String, Lock> entry: locks.entrySet()) {
+                String name = entry.getKey();
+                LockType lockType = entry.getValue().getLockType();
+                embed.addField("", Markdown.link(lockType.getDisplayName(), name) + " Lock: " + lockType.getDisplayName(), false);
             }
             return embed;
         }
     }
 
+    @Getter
     class LockCommand extends Command.BasicLeaf {
-        @Getter
         private final Secured secured = new DiscordUserSecured(DiscordUser.BOT_OWNERS);
-        @Getter
         private final String name = "lock";
-        @Getter
-        private final String description = "Lock selected repository from read or writing, used for maintenance";
-        @Getter
-        private final List<CommandOption<?,?>> options = List.of(repoOption, lockLevelOption);
+        private final String description = "(Un)Lock selected repository from write or readWrite, used for maintenance";
+        private final List<CommandOption<?,?>> options = List.of(repoOption, lockTypeOption);
 
         @NotNull
         @Override
         public SafeMessageBuilder handleEvent(@NotNull ChatInputInteractionEvent event) {
             GitRepository repo = repoOption.get(event);
-            Class<? extends GitRepository.ReadAccess> lockLevel = lockLevelOption.get(event);
-            if (lockLevel == null)
-                lockLevel = GitRepository.ReadWriteAccess.class;
+            LockType lockType = lockTypeOption.get(event);
+            if (lockType == null)
+                lockType = LockType.WRITE;
 
-            GitRepository.ReadAccess oldLock = locks.get(repo.getName());
-            if (oldLock == null || !oldLock.getClass().equals(lockLevel)) {
-                if (oldLock != null) {
-                    oldLock.close();
-                }
-                locks.put(repo.getName(),
-                          lockLevel.equals(GitRepository.ReadAccess.class) ? repo.acquireReadAccess() : repo.acquireWriteAccess());
-                return new MultiMessageSafeEmbedMessageBuilder()
-                    .title("Locked " + repo.getName())
-                    .color(Colors.SUCCESS);
-            }
-            return new MultiMessageSafeEmbedMessageBuilder()
-                .title(repo.getName() + "was already locked")
-                .color(Colors.UNCHANGED);
-        }
-    }
-
-    class UnlockCommand extends Command.BasicLeaf {
-        @Getter
-        private final Secured secured = new DiscordUserSecured(DiscordUser.BOT_OWNERS);
-        @Getter
-        private final String name = "unlock";
-        @Getter
-        private final String description = "Unlock selected repository from read or writing, used for maintenance";
-        @Getter
-        private final List<CommandOption<?,?>> options = List.of(repoOption);
-
-        @NotNull
-        @Override
-        public SafeMessageBuilder handleEvent(@NotNull ChatInputInteractionEvent event) {
-            GitRepository repo = repoOption.get(event);
-
-            GitRepository.ReadAccess oldLock = locks.remove(repo.getName());
-            if (oldLock != null) {
+            Lock oldLock = locks.get(repo.getName());
+            if (oldLock.getLockType() != lockType) {
                 oldLock.close();
+                locks.put(repo.getName(), lockType.makeLock(repo));
                 return new MultiMessageSafeEmbedMessageBuilder()
-                    .title("Unlocked " + repo.getName())
+                    .title((lockType == LockType.NONE ? "Unlocked " : "Locked ") + repo.getName())
                     .color(Colors.SUCCESS);
             }
             return new MultiMessageSafeEmbedMessageBuilder()
-                .title(repo.getName() + " was already unlocked")
+                .title(repo.getName() + " lock state didn't change")
                 .color(Colors.UNCHANGED);
         }
     }
