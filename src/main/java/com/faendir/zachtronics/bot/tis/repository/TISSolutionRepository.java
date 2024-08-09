4f0f6b699ca56b3986434cb2d971d9d60411a26e
Andrea Stacchiotti <crudepatate@gmail.com>
2024-08-09 09:29:06 +0200

    Fix comparison error

--- src/main/java/com/faendir/zachtronics/bot/tis/repository/TISSolutionRepository.java
+++ src/main/java/com/faendir/zachtronics/bot/tis/repository/TISSolutionRepository.java
@@ -87,7 +87,7 @@ public class TISSolutionRepository extends AbstractSolutionRepository<TISCategor
             // s1 dominates
             return -1;
         }
-        else if (r1 >= 0 && r2 >= 0 && r3 >= 0 && r4 >= 0 && r5 >= 0 && r6 <= 0) {
+        else if (r1 >= 0 && r2 >= 0 && r3 >= 0 && r4 >= 0 && r5 >= 0 && r6 >= 0) {
             // s2 dominates
             return 1;
         }
