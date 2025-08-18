49efe79b83b2a0047b7bad3b110629b5c59b8707
Andrea Stacchiotti <crudepatate@gmail.com>
2025-08-18 23:51:09 +0200

    build slightly more better

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024
+ * Copyright (c) 2025
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,10 +31,10 @@ plugins {
 
 allprojects {
     repositories {
-        mavenLocal()
         mavenCentral()
         google()
         maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots") }
+        mavenLocal()
     }
 
     tasks.withType<KotlinCompile> {
